5a83ca082f7235d02124bc056cea4a50d6b57bfc
Magnus Kjelland <magnus@kjelland.com>
2020-04-13 18:05:07 +0200

    styling login

--- src/apps/rooms/index.js
+++ src/apps/rooms/index.js
@@ -42,20 +42,39 @@ class RoomApp extends Component {
 
   renderMessages = () => {
     return this.state.array
-      .map((item, key) => (
-        <li>
-          <div className="row comments mb-2">
-            <div className="col-md-2 col-sm-2 col-3 text-center user-img">
-              <img src="/images/profile-picture.png" alt="user thumbnail profile picture" />
-            </div>
-            <div className="col-md-9 col-sm-9 col-9 comment rounded mb-2">
-          <h4 className="m-0"><a href={`/user/${item.user.uid}`}>{item.user.email}</a></h4>
-              <time className="text-white ml-3">1 hours ago</time>
-              <p className="mb-0 text-white">{item.text}</p>
-            </div>
-          </div>
-        </li>
-      ));
+      .map((item, key) => {
+        if (jsonData.user.uid === item.user.uid) {
+          return (
+            <li>
+              <div className="row comments mb-2">
+                <div className="col-md-9 col-sm-9 col-9 comment rounded mb-2">
+                  <h4 className="m-0"><a href={`/user/${item.user.uid}`}>{item.user.email}</a></h4>
+                  <time className="text-white ml-3">1 hours ago</time>
+                  <p className="mb-0 text-white">{item.text}</p>
+                </div>
+                <div className="col-md-2 col-sm-2 col-3 text-center user-img">
+                  <img src="/images/profile-picture.png" alt="user thumbnail profile picture" />
+                </div>
+              </div>
+            </li>
+          );
+        } else {
+          return (
+            <li>
+              <div className="row comments mb-2">
+                <div className="col-md-2 col-sm-2 col-3 text-center user-img">
+                  <img src="/images/profile-picture.png" alt="user thumbnail profile picture" />
+                </div>
+                <div className="col-md-9 col-sm-9 col-9 comment rounded mb-2">
+                  <h4 className="m-0"><a href={`/user/${item.user.uid}`}>{item.user.email}</a></h4>
+                  <time className="text-white ml-3">1 hours ago</time>
+                  <p className="mb-0 text-white">{item.text}</p>
+                </div>
+              </div>
+            </li>
+          );
+        }
+      });
   }
 
   render() {
